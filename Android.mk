4642a8c0fe2b898d76ff15a79487246bfdc5e5ba
root <root@runnervm5mmn9.thalhjjse5aevfwueukddivcod.cx.internal.cloudapp.net>
2026-07-09 05:32:31 +0000

    hermes: Convert to explicit PRODUCT_COPY_FILES and Soong prebuilt modules
    - Replace ambiguous find-copy-subdir-files with explicit
      PRODUCT_COPY_FILES entries for all 350 proprietary files
    - Add PRODUCT_SOONG_NAMESPACES and PRODUCT_PACKAGES
    - Convert vendor modules from Android.mk to Android.bp
      (cc_prebuilt_library_shared + cc_prebuilt_binary)
    - Clean up Android.mk to empty shell matching reference pattern

--- Android.mk
+++ Android.mk
@@ -1,63 +1,19 @@
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libvcodecdrv
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER := mtk
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libvcodecdrv.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libvcodecdrv.so
-include $(BUILD_PREBUILT)
+ifeq ($(TARGET_DEVICE),hermes)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libion_mtk
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libion_mtk.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libion_mtk.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libcam.utils.sensorlistener
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_OWNER := mtk
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libcam.utils.sensorlistener.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libcam.utils.sensorlistener.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libged
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libged.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libged.so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := librilmtk
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/librilmtk.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/librilmtk.so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := mtk-ril
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/mtk-ril.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/mtk-ril.so
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
+endif
